49a7e96ec28e0d469ea8a99b3a972d9fefa932a0
hjesusmo <heriveltojunior1@gmail.com>
2022-05-24 11:36:11 -0300

    [UPDATE] atualizada as fontes, cores das telas existentes.

--- app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
@@ -21,6 +21,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
 }
