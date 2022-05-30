2fc515f6a6b27d067ee78c3f994502541ef137c8
Samuel0710 <deadpoolscarlet@gmail.com>
2022-05-30 13:40:03 -0300

    [NEW] Adicionado lógica de  splash screen.

--- app/src/main/java/com/example/cryptocurrencyapp/view/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/view/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.cryptocurrencyapp.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cryptocurrencyapp.R
+import com.example.cryptocurrencyapp.R.id.*
 import com.example.cryptocurrencyapp.data.api.retrofit.RetrofitRequestHelper
 import com.example.cryptocurrencyapp.databinding.ActivityMainBinding
 import com.example.cryptocurrencyapp.viewmodel.AssetsListViewModel
@@ -28,6 +29,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupBottomNav()
         binding.mainBottomNavigation.setupWithNavController(controller)
     }
+
+    private fun setupBottomNav() {
+        controller.addOnDestinationChangedListener {_,destination,_ ->
+            when(destination.id){
+                coinList, coinFavorites, coinDetails -> {
+                    visibilityButtonNavigation()
+                }
+                else -> hideButtonNavigation()
+            }
+        }
+    }
+
+    private fun visibilityButtonNavigation() {
+        binding.mainBottomNavigation.visibility = View.VISIBLE
+    }
+    private fun hideButtonNavigation(){
+        binding.mainBottomNavigation.visibility = View.GONE
+    }
 }
+
+
---
+++ app/src/main/java/com/example/cryptocurrencyapp/view/fragments/SplashScreenFragment.kt
@@ -0,0 +1,39 @@
+package com.example.cryptocurrencyapp.view.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.cryptocurrencyapp.R
+import com.example.cryptocurrencyapp.databinding.SplashScreenFragmentBinding
+
+class SplashScreenFragment : Fragment() {
+
+    private lateinit var binding: SplashScreenFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = SplashScreenFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSplashScreen()
+    }
+
+    private fun setupSplashScreen(){
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_to_coinList)
+        }, 3000)
+
+
+    }
+
+}
