7745a21593b3d6f09dec7ad3f18820dd0d173edc
allan8araujo <allanzao08@gmail.com>
2022-06-07 20:18:17 -0300

    [NEW] Algumas animações adicionadas entre a nevagação das fragments.

--- apiLibrary/src/main/java/com/example/apilibrary/repository/const/Constants.kt
+++ apiLibrary/src/main/java/com/example/apilibrary/repository/const/Constants.kt
@@ -5,11 +5,12 @@ class Constants {
         const val PATH_URL_BASE = "https://rest.coinapi.io"
         const val LIST_PATH = "v1/assets"
         const val ICONS_PATH = "v1/assets/icons/128"
-        const val API_KEY = "936151DF-3AB9-4DDE-8EA0-F5FB18F109F9"
+        const val API_KEY = "B866AC1B-48EA-4F5F-AB3A-4463CD622A31"
         const val AMAZON_ICON =
             "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/"
 
 //        const val API_KEY = "C4D1540B-3B30-410C-9D30-1D06A82F678C"
+        //       const val API_KEY = "936151DF-3AB9-4DDE-8EA0-F5FB18F109F9"
 //        const val API_KEY = "C4227E0F-DE5F-4E80-8B53-FA0D24542C28"
 //        const val API_KEY = "0FACCEE3-25E3-4347-B99D-E49D9C11FF8D"
     }
