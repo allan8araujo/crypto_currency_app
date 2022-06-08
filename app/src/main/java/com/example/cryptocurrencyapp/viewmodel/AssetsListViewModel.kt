20f430e56f3ac1ed19ee62ffbc76ce27163b85eb
allan8araujo <allanzao08@gmail.com>
2022-06-08 20:57:53 -0300

    [NEW] Criada teste da função toAssets da viewModel.

--- app/src/main/java/com/example/cryptocurrencyapp/viewmodel/AssetsListViewModel.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/viewmodel/AssetsListViewModel.kt
@@ -9,17 +9,15 @@ import com.example.apilibrary.repository.const.Constants.Companion.AMAZON_ICON
 import com.example.apilibrary.repository.response.AssetsDTO.AssetsDTO
 import com.example.cryptocurrencyapp.models.assets.Assets.AssetsItem
 import com.example.cryptocurrencyapp.models.assets.Assets.funEmptyAssets
-import com.example.cryptocurrencyapp.models.assets.AssetsImage.AssetsImage
 import com.example.cryptocurrencyapp.viewmodel.results.DataResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class AssetsListViewModel(
-
     private val assetsRespository: IAssetsRepository,
 ) : ViewModel() {
-    val liveList = MutableLiveData<DataResult<List<AssetsItem>>>()
+    private val liveList = MutableLiveData<DataResult<List<AssetsItem>>>()
     val assets: LiveData<DataResult<List<AssetsItem>>> = liveList
 
     fun getAllAssets() {
@@ -41,43 +39,43 @@ class AssetsListViewModel(
             liveList.value = assetsFromApi
         }
     }
-
-    private fun AssetsDTO.toAssets(): List<AssetsItem> {
-        return map {
-            AssetsItem(
-                asset_id = it.asset_id,
-                name = it.name,
-                type_is_crypto = it.type_is_crypto,
-                data_quote_start = it.data_quote_start,
-                data_quote_end = it.data_quote_end,
-                data_orderbook_start = it.data_orderbook_start,
-                data_orderbook_end = it.data_orderbook_end,
-                data_trade_start = it.data_trade_start,
-                data_trade_end = it.data_trade_end,
-                data_symbols_count = it.data_symbols_count,
-                volume_1hrs_usd = it.volume_1hrs_usd,
-                volume_1day_usd = it.volume_1day_usd,
-                volume_1mth_usd = it.volume_1mth_usd,
-                price_usd = it.price_usd,
-                id_icon = toAssetsImage(it.id_icon),
-                data_start = it.data_start,
-                data_end = it.data_end
-            )
-        }
-    }
-
-     fun toAssetsImage(idIcon: String?): String? {
-        idIcon?.let {
-            return stringToUrl(idIcon)
-        }
-        return null
-    }
-
-    private fun stringToUrl(idIcon: String): String {
-        return AMAZON_ICON + idIcon.replace("-", "") + ".png"
-    }
-
     fun loadUrlFromGlide(assetItem: AssetsItem): String? {
         return assetItem.id_icon
     }
 }
+
+fun AssetsDTO.toAssets(): List<AssetsItem> {
+    return map {
+        AssetsItem(
+            asset_id = it.asset_id,
+            name = it.name,
+            type_is_crypto = it.type_is_crypto,
+            data_quote_start = it.data_quote_start,
+            data_quote_end = it.data_quote_end,
+            data_orderbook_start = it.data_orderbook_start,
+            data_orderbook_end = it.data_orderbook_end,
+            data_trade_start = it.data_trade_start,
+            data_trade_end = it.data_trade_end,
+            data_symbols_count = it.data_symbols_count,
+            volume_1hrs_usd = it.volume_1hrs_usd,
+            volume_1day_usd = it.volume_1day_usd,
+            volume_1mth_usd = it.volume_1mth_usd,
+            price_usd = it.price_usd,
+            id_icon = toAssetsImage(it.id_icon),
+            data_start = it.data_start,
+            data_end = it.data_end
+        )
+    }
+}
+
+fun toAssetsImage(idIcon: String?): String? {
+    idIcon?.let {
+        return stringToUrl(idIcon)
+    }
+    return null
+}
+
+fun stringToUrl(idIcon: String): String {
+    return AMAZON_ICON + idIcon.replace("-", "") + ".png"
+}
+
--- app/src/test/java/com/example/cryptocurrencyapp/viewmodel/AssetsListViewModelTest.kt
+++ app/src/test/java/com/example/cryptocurrencyapp/viewmodel/AssetsListViewModelTest.kt
@@ -1,26 +1,38 @@
 package com.example.cryptocurrencyapp.viewmodel
 
+import com.example.apilibrary.repository.response.AssetsDTO.AssetsDTO
+import com.example.apilibrary.repository.response.AssetsDTO.funMockAssets
+import org.junit.Assert
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.*
 
 internal class AssetsListViewModelTest {
+
+//    @Test
+//    fun givenServerResponseError_whenFetch_shouldReturnError() {
+//        testCoroutineRule.runBlockingTest {
+//            val someGeekyError = "Something is not right"
+//            doThrow(RuntimeException(someGeekyError))
+//                .`when`(gfgApi)
+//                .getGfgUser()
+//            val viewModel = SingleNetworkCallViewModel(gfgApi, gfgDBHelper)
+//            viewModel.getGfgUser().observeForever(apiUsersObserver)
+//            verify(gfgApi).getGfgUser()
+//            verify(apiUsersObserver).onChanged(
+//                Resource.error(
+//                    RuntimeException(someGeekyError).toString(),
+//                    null
+//                )
+//            )
+//            viewModel.getGfgUser().removeObserver(apiUsersObserver)
+//        }
+//    }
     @Test
-    fun givenServerResponseError_whenFetch_shouldReturnError() {
-        testCoroutineRule.runBlockingTest {
-            val someGeekyError = "Something is not right"
-            doThrow(RuntimeException(someGeekyError))
-                .`when`(gfgApi)
-                .getGfgUser()
-            val viewModel = SingleNetworkCallViewModel(gfgApi, gfgDBHelper)
-            viewModel.getGfgUser().observeForever(apiUsersObserver)
-            verify(gfgApi).getGfgUser()
-            verify(apiUsersObserver).onChanged(
-                Resource.error(
-                    RuntimeException(someGeekyError).toString(),
-                    null
-                )
-            )
-            viewModel.getGfgUser().removeObserver(apiUsersObserver)
-        }
+    fun `ao chamar a funcao toAssets com dados nao vazios, o resultado precisa ser diferente de nulo ou vazio`() {
+        val assetDTO = AssetsDTO()
+        assetDTO.addAll(
+            funMockAssets()
+        )
+        val asset = assetDTO.toAssets()
+        Assert.assertFalse(asset.isNullOrEmpty())
     }
 }
