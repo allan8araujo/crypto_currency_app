b13e38ec1ec611f2e9b4c008a8660b0ce09e8548
allan8araujo <allanzao08@gmail.com>
2022-06-13 10:57:17 -0300

    [FIX] Bugfix into AssetsRequestTest.kt.

--- apiLibrary/src/test/java/com/example/apilibrary/repository/api/AssetsRequestTest.kt
+++ apiLibrary/src/test/java/com/example/apilibrary/repository/api/AssetsRequestTest.kt
@@ -1,16 +1,20 @@
 package com.example.apilibrary.repository.api
 
 import com.example.apilibrary.repository.api.retrofit.RetrofitRequestHelper
+import com.example.apilibrary.repository.const.Constants
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
 internal class AssetsRequestTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testApi() {
         runTest {
+            Constants.API_KEY = "0FACCEE3-25E3-4347-B99D-E49D9C11FF8D"
             Assert.assertNotEquals(
-                0, RetrofitRequestHelper().getListAssets().getAssets().size
+                0, RetrofitRequestHelper.getListAssets().getAssets()
             )
         }
     }
