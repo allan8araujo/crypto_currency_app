b84f445c7d706c5cc9e218d445bc107d99312a44
allan8araujo <allanzao08@gmail.com>
2022-06-22 13:47:37 -0300

    [FIX] Modularizando pelo repository ao invés de apiLibrary

--- repository/build/tmp/kapt3/stubs/debug/com/example/apilibrary/repository/api/request/AssetsRequest.java
+++ repository/build/tmp/kapt3/stubs/debug/com/example/apilibrary/repository/api/request/AssetsRequest.java
@@ -2,7 +2,7 @@ package com.example.apilibrary.repository.api.request;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0011\u0010\u0005\u001a\u00020\u0006H\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0007R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\b"}, d2 = {"Lcom/example/apilibrary/repository/api/request/AssetsRequest;", "Lcom/example/apilibrary/repository/api/request/IAssetsRequest;", "assetsClient", "Lcom/example/apilibrary/repository/api/retrofit/RetrofitService;", "(Lcom/example/apilibrary/repository/api/retrofit/RetrofitService;)V", "getAssets", "Lcom/example/apilibrary/repository/response/AssetsDTO/AssetsDTO;", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "repository_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0011\u0010\u0005\u001a\u00020\u0006H\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0007R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\b"}, d2 = {"Lcom/example/apilibrary/repository/api/request/AssetsRequest;", "Lcom/example/apilibrary/repository/api/request/IAssetsRequest;", "assetsClient", "Lcom/example/apilibrary/repository/api/retrofit/RetrofitService;", "(Lcom/example/apilibrary/repository/api/retrofit/RetrofitService;)V", "getAssets", "Lcom/example/abstraction/Assets;", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "repository_debug"})
 public final class AssetsRequest implements com.example.apilibrary.repository.api.request.IAssetsRequest {
     private final com.example.apilibrary.repository.api.retrofit.RetrofitService assetsClient = null;
     
@@ -14,7 +14,7 @@ public final class AssetsRequest implements com.example.apilibrary.repository.ap
     @org.jetbrains.annotations.Nullable()
     @java.lang.Override()
     public java.lang.Object getAssets(@org.jetbrains.annotations.NotNull()
-    kotlin.coroutines.Continuation<? super com.example.apilibrary.repository.response.AssetsDTO.AssetsDTO> continuation) {
+    kotlin.coroutines.Continuation<? super com.example.abstraction.Assets> continuation) {
         return null;
     }
 }
