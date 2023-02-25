90f7c3523a09d0285838eef727ad11d846da058c
allan <allan8araujo@gmail.com>
2023-02-24 21:35:19 -0300

    fix link

--- repository/src/main/java/com/example/apilibrary/repository/const/Constants.kt
+++ repository/src/main/java/com/example/apilibrary/repository/const/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
     companion object {
         const val DATABASE_DIR = "favorites.txt"
         val DATE_NOW = SimpleDateFormat("dd MMM yyyy").format(Date())
-        const val PATH_URL_BASE = "https://rest.coinapi.io/v1/management/subscriptions"
+        const val PATH_URL_BASE = "https://rest.coinapi.io/"
         const val LIST_PATH = "v1/assets"
         const val ICONS_PATH = "v1/assets/icons/128"
         const val AMAZON_ICON =
