18c76dbb2a77528b76323d4cb26da615086a6dd6
allan <allan8araujo@gmail.com>
2023-04-01 14:06:51 -0300

    remove unused imports

--- repository/src/main/java/com/example/apilibrary/repository/api/retrofit/RetrofitService.kt
+++ repository/src/main/java/com/example/apilibrary/repository/api/retrofit/RetrofitService.kt
@@ -1,11 +1,8 @@
 package com.example.apilibrary.repository.api.retrofit
 
 import com.example.abstraction.Assets
-import com.example.abstraction.AssetCecko
 import com.example.apilibrary.repository.const.Constants
-import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface RetrofitService {
     @GET(Constants.LIST_PATH)
